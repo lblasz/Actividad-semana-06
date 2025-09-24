8f89c99d705f89ec4b97ac60c72eaa1f5e75416f
lblasz <kameron.rivera.1234@gmail.com>
2025-09-23 21:05:29 -0300

    ahora al presionar el boton de hacer foto en el main activity te lleva al activity inicio

--- app/src/main/java/com/example/actividadsemana06/Inicio.java
+++ app/src/main/java/com/example/actividadsemana06/Inicio.java
@@ -1,4 +1,37 @@
 package com.example.actividadsemana06;
 
-public class Inicio {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Inicio extends AppCompatActivity {
+
+    Button btnAceptar;
+    EditText editTextNombre;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+        btnAceptar = findViewById(R.id.btnAceptar);
+        editTextNombre = findViewById(R.id.editTextNombre);
+        imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.ic_launcher_background);
+
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String stNombre = editTextNombre.getText().toString();
+               Toast.makeText(Inicio.this, stNombre, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/actividadsemana06/MainActivity.java
+++ app/src/main/java/com/example/actividadsemana06/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.actividadsemana06;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +16,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnFoto;
+    ImageView imageView2;
+    TextView textView2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btnFoto = findViewById(R.id.btnFoto);
+         imageView2 = findViewById(R.id.imageView2);
+        textView2 = findViewById(R.id.textView2);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Inicio.class);
+                startActivity(intent);
+            }
         });
     }
 }
